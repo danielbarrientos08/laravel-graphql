d3fa4e8ad0149f8a3630282258bd4e1a1a8a6fa3
daniel barrientos <daniel.barrientos@delfosti.com>
2023-07-12 10:04:58 -0500

    Add Types

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'post_id' => Post::factory(),
+            'reply' => $this->faker->text(30),
+        ];
+    }
+}
